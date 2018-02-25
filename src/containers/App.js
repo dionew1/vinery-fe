dcc61c08620279a265a36c684859d021b30e4576
Dione Wilson <dionew1@gmail.com>
2018-02-24 22:33:03 -0700

    Add navigation to header.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
 import HomePage from '../components/HomePage'
 import WinesContainer from './WinesContainer'
 
@@ -18,6 +18,13 @@ class App extends Component {
         <header className="app-header">
           <img className="app-glass" src="favicon.ico" alt="Wine Glass"/>
           <h1 className="app-title">Welcome to Vinery</h1>
+          <BrowserRouter>
+            <nav>
+              <Link to="/wine-categories"> Wine Categories </Link>
+              <Link to="/wines"> Wines </Link>
+              <Link to="/foods"> Food Types </Link>
+            </nav>
+          </BrowserRouter>
         </header>
         <main>
           <BrowserRouter>
