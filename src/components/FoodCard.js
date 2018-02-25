27f29a3cd7bc01d93895df32d2095f881045cd27
Dione Wilson <dionew1@gmail.com>
2018-02-25 16:08:57 -0700

    Add styling to wine categories page.

--- src/components/FoodCard.js
+++ src/components/FoodCard.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+
+const createAltTag = (food) => {
+  return(
+    food.image_url.split("/").pop()
+  )
+}
+
 const FoodCard = (props) => {
   let food = props.food
   return(
     <div className="food-row">
       <h1 className="food-name">{ food.name }</h1>
+      <img className="food-image"
+        src={ food.image_url }
+        alt={ createAltTag(food) } />
     </div>
   )
 }
