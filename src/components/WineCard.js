2f676eafd12dd404ff867c3a8832ac88b3d6442e
Dione Wilson <dionew1@gmail.com>
2018-02-25 19:27:25 -0700

    Style Wine Cards

--- src/components/WineCard.js
+++ src/components/WineCard.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import '../styles/Wine.css'
 
 const WineCard = (props) => {
   let wine = props.wine
   return(
     <div className="wine-row">
-      <h1 className="wine-name">{ wine.name }</h1>
-      <p className="wine-description">{ wine.description }</p>
       <img className="wine-image"
-        src="/wine-glass-by-kym-ellis.jpg"
-        alt="Wine glass overlooking vinyard by Kym Ellis"/>
+      src="/wine-glass-by-kym-ellis.jpg"
+      alt="Wine glass overlooking vinyard by Kym Ellis"/>
+      <div className="wine-details">
+        <div className="wine-detail-module">
+          <h1 className="wine-name">{ wine.name }</h1>
+          <p className="wine-description">{ wine.description }</p>
+        </div>
+      </div>
     </div>
   )
 }
